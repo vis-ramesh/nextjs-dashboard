82e5ae46c2d3e38cdacb612afe159e89e5722787
Ramesh <vis.rameshh@gmail.com>
2024-09-18 14:22:15 +0400

    nav

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -13,7 +13,11 @@ const links = [
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { 
+    name: 'Customers', 
+    href: '/dashboard/customers', 
+    icon: UserGroupIcon 
+  },
 ];
 
 export default function NavLinks() {
